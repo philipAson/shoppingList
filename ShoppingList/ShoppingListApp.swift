6d3cf9742e1a969235021d8c1239b07bfc931118
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-04-17 09:52:09 +0200

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Philip Andersson on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
